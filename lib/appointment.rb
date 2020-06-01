c89a8502f88149a7dd1671c08a45e1cc12319d60
Kevin Boston <kbos22@gmail.com>
2020-06-01 20:48:01 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,3 +2,9 @@ class Appointment
   @@all = []
   attr_accessor :date, :patient, :doctor 
   def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient
+    @doctor = doctor
+    @@all << self 
+  end
+end
