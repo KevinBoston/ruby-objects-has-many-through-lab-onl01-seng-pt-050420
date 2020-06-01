d9f551a7ffd85ea582554833953c17ef8b5bff12
Kevin Boston <kbos22@gmail.com>
2020-06-01 20:23:51 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,10 @@
+class Patient 
+  @@all = []
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
